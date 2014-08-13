9bc842e92005565647bf93d48e57f9c20d79453f
Leon Bentrup <leon.bentrup@1und1.de>
2014-08-13 13:55:31 +0200

    Bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
 	name = 'Dolly',
 	packages = ['dolly'],
-	version = '0.0.2',
+	version = '0.0.3',
 	install_requires=['pyyaml', 'argparse'],
 	entry_points={
 		'console_scripts': [
