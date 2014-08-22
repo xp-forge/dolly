a4bcb6188fb4a2a0ceb1c4e1155cbb8f5683f288
Leon Bentrup <leon.bentrup@1und1.de>
2014-08-22 11:47:23 +0200

    Add README.md to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@
 from setuptools import setup
 setup(
 	name = 'Dolly',
+	description='Dolly manages multiple git and svn repos'
+	long_description=open('README.md').read(),
 	packages = ['dolly'],
 	version = '0.0.9',
 	install_requires=['pyyaml', 'argparse'],
