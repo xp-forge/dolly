89534947d6d727d4bc00f129e757e88bd0fe0a98
Lukas Werling <lukas.werling@1und1.de>
2014-11-28 09:05:21 +0100

    Reduce code duplication in the Install visitor

--- dolly/v_install.py
+++ dolly/v_install.py
@@ -4,29 +4,9 @@
 import os
 import util
 import project
+import v_update
 
-class Install:
-	def visit(self, host):
-		for repo in host.tree:
-			project.Project.currentProj += 1
-			util.printStatus(repo)
-			self.update(repo)
-
-		if host.post_update:
-			util.executeCommand(host.post_update)
-	
-	def update(self, repo):
-		if not os.path.exists(repo['local']):
-			self.clone(repo)
-
-	def clone(self, repo):
-		if util.isGitRepo(repo):
-			self.cloneGit(repo)
-		else:
-			self.cloneSvn(repo)
-
-	def cloneGit(self, repo):
-		result = util.executeCommand('git clone {0} {1}'.format(repo['remote'], repo['local']))
-
-	def cloneSvn(self, repo):
-		result = util.executeCommand('svn checkout --config-option servers:global:store-plaintext-passwords=yes {0} {1}'.format(repo['remote'], repo['local']))
+# Install is like update, but doesn't perform pulls.
+class Install(v_update.Update):
+	def pull(self, repo):
+		return
