2b1dc8272abfaf76cb616ffbf5d41f375638e2fa
Lukas Werling <lukas.werling@1und1.de>
2015-10-30 11:27:41 +0100

    Fix race condition while creating parent directories

--- dolly/gitrepository.py
+++ dolly/gitrepository.py
@@ -1,12 +1,28 @@
 from repository import Repository
 import util
 
+import os, errno
+
+# Tries to create parent directories, avoiding race conditions.
+#
+# Git will do this automatically, but will fail when the directories are
+# created in parallel.
+def create_parent_directories(path):
+	dir = os.path.dirname(path)
+	if not os.path.exists(dir):
+		try:
+			os.makedirs(dir)
+		except OSError, e:
+			# We could check e.errno here for errno.EEXIST, but git will fail for all other errors anyways.
+			return
+
 class GitRepository(Repository):
 	def __init__(self, repo):
 		super(GitRepository, self).__init__(repo)
 
 	def clone(self):
 		repo = self.data
+		create_parent_directories(repo['local'])
 		branch = repo['branch']
 		if repo['tag'] != '':
 			branch = repo['tag']
